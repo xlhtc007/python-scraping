c925c687c739a6a13cc0bedbb0315ed4f19025a4
Lynnhom <xlh1221it@gmail.com>
2017-06-14 23:24:53 +0800

    add phantomjs

--- chapter1/1-basicExample.py
+++ chapter1/1-basicExample.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+from urllib import urlopen
 #Retrieve HTML string from the URL
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 print(html.read())
